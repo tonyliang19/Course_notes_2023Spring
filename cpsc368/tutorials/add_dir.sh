827cd4cd3171490fbd89b2e7ca703489354eda5d
Tony Liang <chunqingliang@gmail.com>
2023-03-04 19:21:10 -0800

    updated add_dir.sh

--- cpsc368/tutorials/add_dir.sh
+++ cpsc368/tutorials/add_dir.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 DIRECTORY" >&2
+  exit 1
+fi
+if ! [ -e "$1" ]; then
+  echo "$1 not found" >&2
+  exit 1
+fi
+if ! [ -d "$1" ]; then
+  echo "$1 not a directory" >&2
+  exit 1
+fi
+
+
 
 echo " This script moves everything in a download dir (defined by user), and named by $(1)"
 
@@ -16,11 +30,11 @@ ls ${DOWNLOAD}
 # make the directory first
 if [ -d ${1} ]
 then
-	mv ${DOWNLOAD}/!(desktop.ini) $(pwd)/${1}
+	mv ${DOWNLOAD}/^desktop.ini $(pwd)/${1}
 	echo "Files moved to ${1}"
 else
 	mkdir -p ${1}
 	echo "${1} did not exists, created now"
-	mv ${DOWNLOAD}/!(dekstop.ini) $(pwd)/${1}
+	mv ${DOWNLOAD}/^dekstop.ini $(pwd)/${1}
 	echo "Files sucessfully moved in ${1}"
 fi
